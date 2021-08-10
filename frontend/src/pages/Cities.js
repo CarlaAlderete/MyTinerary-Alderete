c7aa08a142a129f2f0e6bdf17b541582352cead6
Carla <carlamalderete@gmail.com>
2021-08-10 15:24:29 -0300

    10-08-15:24

--- frontend/src/pages/Cities.js
+++ frontend/src/pages/Cities.js
@@ -2,6 +2,20 @@ import Header from "../components/Header"
 import Footer from "../components/Footer"
 import {Component} from "react"
 
+const newcities = [
+    [{ foto:'copenhagen.jpg',city: 'Copenhagen'},
+      { foto:'Boracay.jpg', city: 'Boracay'},
+      { foto:'Rio.jpg', city: 'Rio de Janeiro'},
+      { foto:'Reykjavik.jpg',city: 'Reykjavik'}],
+    [{ foto:'amsterdam.jpg',city: 'Amsterdam'},
+      { foto:'londres.jpg',city: 'London'},
+      { foto:'Mykonos.jpg',city: 'Mykonos'},
+      { foto:'toronto.jpg',city: 'Toronto'}],
+    [{ foto:'telaviv.jpg',city: 'Tel aviv'},
+      { foto:'losangeles.jpg',city: 'Los Angeles'},
+      { foto:'ny.jpg',city: 'New York'},
+      { foto:'sanfrancisco.jpg',city: 'San Francisco'}]
+];
 
 export default class Cities extends Component{
     render(){
