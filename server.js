6a81267b91f9efb23af39925606c28307bd1ecd4
Carla <carlamalderete@gmail.com>
2021-09-20 19:52:07 -0300

    hosting

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ require('dotenv').config()
 const router = require('./routes/index')
 require('./config/database')
 require('./config/passport')
+const path = require('path')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.use('/api', router)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+       res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    })
+}
 
-app.listen(4000, () => console.log('Server listening on port 4000'))
+app.listen(process.env.PORT || 4000, process.env.MYHOST || '0.0.0.0', () => console.log("Server listening on port 4000"))
